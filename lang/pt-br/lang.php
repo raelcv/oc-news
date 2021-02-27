a8dc3c69682f2c629aab314e94ed52c000659b93
raelcv <raelcv@gmail.com>
2021-02-26 23:03:59 -0300

    Update lang.php

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -121,7 +121,7 @@ return [
         'deactivate' => 'Postagens desativadas com sucesso.',
         'draft' => 'Postagens modificadas como rascunho com sucesso.',
         'subscribe' => 'Inscrições efetuadas com sucesso.',
-        'unsubscribe' => 'A inscrição dos usuários foram canceladas com sucesso.',
+        'unsubscribe' => 'As inscrições dos usuários foram canceladas com sucesso.',
         'delete' => 'Você deseja deletar estes itens?',
         'remove' => 'Itens removidos com sucesso.',
         'newsletter_test_error' => 'Um erro ocorreu ao enviar a newsletter de teste.',
